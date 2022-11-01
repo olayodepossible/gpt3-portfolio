0b118ab16884ef61735b3edfbc208e548ebbff14
Akin <>
2022-11-01 07:17:47 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Blog, Features, Footer, GPT3, Header, Possibility } from "./containers";
+import { CTA, Brand, NavBar } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient__bg">
+        <NavBar />
+        <Header />
+      </div>
+      <Brand />
+      <GPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/article/Artcle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Artcle = () => {
+  return <div>Artcle</div>;
+};
+
+export default Artcle;
