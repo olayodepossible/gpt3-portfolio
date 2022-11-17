ebbb1960e2f4fd0fa1cb65aa3077033c4a38fce4
Akin <>
2022-11-17 19:50:00 +0100

    implement footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import gpt3Logo from "../../assets/Ai_logo.svg";
+import "./Footer.css";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>
+          Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved
+        </p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Checkout layout K12 182 DK Alankara</p>
+        <p>085-132567</p>
+        <p>info@payme.net</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2022 weCode. All rights reserved.</p>
+    </div>
+  </div>
+);
 
 export default Footer;
